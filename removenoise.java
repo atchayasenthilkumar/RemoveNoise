75a0de26d9e1ef9ecc041e289058841f29f8eff8
atchayasenthilkumar <165780325+atchayasenthilkumar@users.noreply.github.com>
2024-09-27 14:55:38 +0530

    Add files via upload
     This program is used for removing additional noise like special characters .

---
+++ removenoise.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+import java.lang.*;
+
+ public class removenoise 
+{
+ 
+ public static void main(String[] args)
+{
+  
+  String text = " apple#$5is@39a+*@fruit";
+  
+ 
+  String text1 = text.replaceAll("[^a-zA-Z0-9]", "");
+
+ 
+  String text2 = text1.replaceAll("\\d","");
+    
+
+    System.out.println(text2);
+ 
+ }
+
+}
+
